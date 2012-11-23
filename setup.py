d066a2608be2234cf5899944010677e865dac3d9
Andrea Cecchi <andrea.cecchi85@gmail.com>
2012-11-23 14:22:07 +0100

    renamed readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.1.1.dev0'
 setup(name='collective.portlet.discussion',
       version=version,
       description="A simple portlet to show a list of comments",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
