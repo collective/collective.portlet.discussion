6f79057d0c8bb97dd6a2e23a3f2e3bf492234709
Luca Fabbri <luca@keul.it>
2013-06-26 16:31:42 +0200

    fixed project description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '1.2.1.dev0'
 
 setup(name='collective.portlet.discussion',
       version=version,
-      description="A simple portlet to show a list of comments",
+      description="A simple Plone portlet to show a list of comments",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
