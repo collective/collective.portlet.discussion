e97657e6f448a931288589639c01046871bef423
Luca Fabbri <luca@keul.it>
2013-06-26 16:25:18 +0200

    new translation messages

--- collective/portlet/discussion/i18n.sh
+++ collective/portlet/discussion/i18n.sh
@@ -3,7 +3,7 @@
 DOMAIN='collective.portlet.discussion'
 
 i18ndude rebuild-pot --pot locales/${DOMAIN}.pot --create ${DOMAIN} .
-
+i18ndude merge --pot locales/${DOMAIN}.pot --merge locales/${DOMAIN}-manual.pot
 i18ndude sync --pot locales/${DOMAIN}.pot locales/*/LC_MESSAGES/${DOMAIN}.po
 
 # Compile po files
